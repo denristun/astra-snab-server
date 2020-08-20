4352867822c06e659dbef5b9deea9e90fc831d07
Mehtahudinov Denis <denristun@gmail.com>
2020-08-20 12:28:22 +0400

    changed port

--- src/server.ts
+++ src/server.ts
@@ -73,7 +73,6 @@ app.use(function (
   res.send({ error: err.message })
   return
 })
-
-app.listen(8000, function () {
-  console.log('App is listening on port 8000!')
+const PORT = process.env.PORT || 8000
+app.listen(PORT, function () {
 })

5d924013c493f2ffb777212a06b0354cf3158389
denristun <58968932+denristun@users.noreply.github.com>
2020-08-28 17:20:28 +0400

    deploying

--- src/server.ts
+++ src/server.ts
@@ -45,7 +45,7 @@ app.get('/api/groups', function (req, res) {
 app.get('/api/unique', async function (req, res) {
   const requests = await BankRequestModel.find({})
   const groups = await GroupModel.find({})
-  const requestsNums = new Set(requests.map(request => request.request))
+  const requestsNums = requests.map(request => request.request)
 
   
   res.send(JSON.stringify({requestsNums, groups}))
